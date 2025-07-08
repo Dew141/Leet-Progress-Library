f2ef5ca3c6417aa92e724a0404ad71014a6cd957
Ryan He <rhe@aischool.org>
2025-07-08 22:57:28 +0800

    Added remove duplicates from sorted array to easy folder

---
+++ easy/remove_dup_from_sorted.py
@@ -0,0 +1,20 @@
+# Leetcode problem #26:
+# URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+# Difficulty: Easy
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+        # should have been done with a set() but works in the same way
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] not in dict:
+                dict[nums[i]] = 1
+                nums[k] = nums[i]
+                k += 1
+        return k
