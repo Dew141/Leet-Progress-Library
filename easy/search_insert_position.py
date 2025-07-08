01b6237f78e97d2dc5dc1d95bb102813632898e4
Ryan He <rhe@aischool.org>
2025-07-09 01:11:03 +0800

    Added search insert position to the easy folder

---
+++ easy/search_insert_position.py
@@ -0,0 +1,22 @@
+# Leetcode problem #35:
+# URL: https://leetcode.com/problems/search-insert-position/
+# Difficulty: Easy
+
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while (left <= right):
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                return mid
+        return left
+            
