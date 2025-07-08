7816f67f050879083069f9721c326f282fd4f369
Ryan He <rhe@aischool.org>
2025-07-08 21:56:45 +0800

    Added palindrome_number to the easy folder

---
+++ easy/palindrome_number.py
@@ -0,0 +1,23 @@
+# Leetcode problem #9:
+# URL:
+# Difficulty: Easy
+
+# Time Complexity: O(log(n))
+# Space Complexity: O(1)
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Negative numbers and numbers ending in 0 (but not 0 itself) can't be palindromes
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # For even digits: x == reversed_half
+        # For odd digits: x == reversed_half // 10 (ignore middle digit)
+        return x == reversed_half or x == reversed_half // 10
+
+
