3c1e7944cd8e5421d3544761a79a62cc8f9da821
Ryan He <rhe@aischool.org>
2025-07-09 18:34:51 +0800

    added plus one solution to the easy folder

---
+++ easy/plus_one.py
@@ -0,0 +1,27 @@
+# Leetcode problem #66:
+# URL: https://leetcode.com/problems/plus-one/description/
+# Difficulty: Easy
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # Perform the + 1 on the last digit
+        digits[-1] += 1
+
+        for i in range(len(digits) - 1, -1, -1):
+            # if there is no carry, no further change necessary
+            if digits[i] < 10:
+                return digits
+            digits[i] = 0
+            # Case if we reach the end with a carry
+            if i == 0:
+                digits.insert(0, 1)
+            # Otherwise, propogate the carry through
+            else:
+                digits[i - 1] += 1
+        # will only return once it ends
+        return digits
