56101123ad0cbc01d83ba3ff8eddb71165e231d1
Ryan He <rhe@aischool.org>
2025-07-09 18:28:01 +0800

    Added length of last word to the easy folder

---
+++ easy/len_last_word.py
@@ -0,0 +1,16 @@
+# Leetcode problem #58:
+# URL: https://leetcode.com/problems/length-of-last-word/
+# Difficulty: Easy
+
+# Time Complexity: O(m) where m is the length of the last word; O(n) worst case 
+# Space Complexity: O(1)
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        str_length = 0
+        for i in reversed(s):
+            if i == ' ' and str_length != 0:
+                break
+            elif i != ' ':
+                str_length += 1
+        return str_length
